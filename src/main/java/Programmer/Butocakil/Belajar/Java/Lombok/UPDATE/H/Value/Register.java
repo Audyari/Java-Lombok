087a4d18c9f3abc3f53086b1350928e677b6b00c
Audyari Wiyono <audy123ari@gmail.com>
2024-09-22 12:02:04 +0700

    8. value imutable class di lombok

---
+++ src/main/java/Programmer/Butocakil/Belajar/Java/Lombok/UPDATE/H/Value/Register.java
@@ -0,0 +1,14 @@
+package Programmer.Butocakil.Belajar.Java.Lombok.UPDATE.H.Value;
+
+import lombok.Setter;
+import lombok.Value;
+
+@Value
+
+public class Register {
+
+
+    String username;
+
+    String password;
+}
---
+++ src/test/java/Programmer/Butocakil/Belajar/Java/Lombok/UPDATE/H/Value/testingRegister.java
@@ -0,0 +1,15 @@
+package Programmer.Butocakil.Belajar.Java.Lombok.UPDATE.H.Value;
+
+import org.junit.jupiter.api.Test;
+
+public class testingRegister {
+
+    @Test
+    void testingRegister() {
+        Register register = new Register("AUDY", "ASEP");
+
+
+
+        System.out.println(register);
+    }
+}
